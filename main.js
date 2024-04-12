a07224ef6f43d248810711e320ccfd3c399ff64e
Ziad Wael <76755899+ziaadwael@users.noreply.github.com>
2024-04-12 22:36:45 +0200

    finished Project And Responsive For All Screens <3

---
+++ main.js
@@ -0,0 +1,14 @@
+// Search Container
+
+document.getElementById("search_icon").addEventListener("click", () => {
+    document.getElementById("search-container").classList.toggle("active")
+})
+
+// Cart Sidebar Container
+document.getElementById("cart_icon").addEventListener("click", () => {
+    document.getElementById("cart-sidebar-container").classList.toggle("active")
+})
+// Small Screens Navbar
+document.getElementById("navbar_icon").addEventListener("click", () => {
+    document.getElementById("nav-sidebar").classList.toggle("active")
+})
